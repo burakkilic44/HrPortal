da56242647d302f86523c13f19755685ac229dab
Murat Demirci <mdemirci@outlook.com>
2017-11-07 10:37:54 +0300

    first version

--- HrPortal/Startup.cs
+++ HrPortal/Startup.cs
@@ -60,6 +60,10 @@ namespace HrPortal
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                     name: "areaRoute",
+                     template: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
