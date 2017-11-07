6af929e5ef69d94e805673994340e6733c7a2c2f
Göksel Yüce <gokselyuce@windowslive.com>
2017-11-07 23:56:31 +0300

    route düzenlemesi

--- HrPortal/Areas/Admin/Controllers/LoginController.cs
+++ HrPortal/Areas/Admin/Controllers/LoginController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HrPortal.Areas.Admin.Controllers
 {
     [Area("admin")]
-    [Route("admin/[controller]")]
+   
     public class LoginController : Controller
     {
         public IActionResult Index()
