0df4414e40298badad75fbffa076093241859094
Mustafa Fındık <mustafa.findik3@gmail.com>
2017-11-07 13:05:09 +0300

    resume to resumes

--- HrPortal/Controllers/ResumesController.cs
+++ HrPortal/Controllers/ResumesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HrPortal.Controllers
 {
-    public class ResumeController : Controller
+    public class ResumesController : Controller
     {
         public IActionResult Index()
         {
