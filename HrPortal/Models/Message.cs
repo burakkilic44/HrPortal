235cfa656c79cbb8361a78065155c43accc8c595
Admin <Admin@DESKTOP-SB3MC0N>
2017-11-08 11:04:31 +0300

    message alanı düzenlendi

--- HrPortal/Models/Message.cs
+++ HrPortal/Models/Message.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace HrPortal.Models
 {
-    public class Messages:BaseEntity
+    public class Message:BaseEntity
     {
+        [Required]
         [Display(Name ="Gönderen Adı")]
         [StringLength(200)]
         public string SenderName { get; set; }
+
+        [Required]
         [Display(Name = "Gönderen Email")]
         [StringLength(200)]
         public string SenderEmail { get; set; }
+
+        [Required]
         [Display(Name = "Gönderen Telefon")]
         [StringLength(200)]
         public string  SenderPhone { get; set; }
+
+        [Required]
         [Display(Name = "Mesaj")]
         [StringLength(4000)]
         public string Message { get; set; }
