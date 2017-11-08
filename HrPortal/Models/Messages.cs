46a24573e1cf77d64f3b74abf9a014269c17ae09
Admin <Admin@DESKTOP-KGJ5C84>
2017-11-08 10:47:41 +0300

    Add Messages

---
+++ HrPortal/Models/Messages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HrPortal.Models
+{
+    public class Messages:BaseEntity
+    {
+        [Display(Name ="Gönderen Adı")]
+        [StringLength(200)]
+        public string SenderName { get; set; }
+        [Display(Name = "Gönderen Email")]
+        [StringLength(200)]
+        public string SenderEmail { get; set; }
+        [Display(Name = "Gönderen Telefon")]
+        [StringLength(200)]
+        public string  SenderPhone { get; set; }
+        [Display(Name = "Mesaj")]
+        [StringLength(4000)]
+        public string Message { get; set; }
+       
+
+
+    }
+}
