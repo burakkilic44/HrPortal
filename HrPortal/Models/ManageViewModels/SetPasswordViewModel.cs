8f720552338b3d7b3672f411e783afb2e591fec9
busrakarnucu <busrakarnucu@gmail.com>
2017-11-09 14:24:10 +0300

    Kullanıcı profiline türkçe alanlar eklendi

--- HrPortal/Models/ManageViewModels/SetPasswordViewModel.cs
+++ HrPortal/Models/ManageViewModels/SetPasswordViewModel.cs
@@ -9,14 +9,14 @@ namespace HrPortal.Models.ManageViewModels
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "Şifre {0} en az  {2} en fazla {1} karakter uzunluğunda olmalıdır.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Yeni Şifre")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Yeni Şifreyi Doğrula")]
+        [Compare("NewPassword", ErrorMessage = "Yeni şifre ve onay şifresi uyuşmuyor.")]
         public string ConfirmPassword { get; set; }
 
         public string StatusMessage { get; set; }
