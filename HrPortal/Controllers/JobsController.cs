341336e9074c6f21d4acb183fad5117f5628e4b0
Murat Demirci <mdemirci@outlook.com>
2017-11-09 11:31:30 +0300

    jobs controller constructor

--- HrPortal/Controllers/JobsController.cs
+++ HrPortal/Controllers/JobsController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HrPortal.Services;
+using HrPortal.Models;
 
 namespace HrPortal.Controllers
 {
     public class JobsController : Controller
     {
+        private IRepository<Job> jobRepository;
+        public JobsController (IRepository<Job> jobRepository)
+        {
+            this.jobRepository = jobRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            var jobs = jobRepository.GetAll();
+            return View(jobs);
 
         }
         public IActionResult Create()
