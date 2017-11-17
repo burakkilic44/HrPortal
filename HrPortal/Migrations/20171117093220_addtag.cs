5a445f1ec418aa7c8748fef5ae285b1412002530
ILKAYBURAK <ILKAYBURAK@DESKTOP-LU5PJ3S>
2017-11-17 12:34:28 +0300

    update Tag

---
+++ HrPortal/Migrations/20171117093220_addtag.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HrPortal.Migrations
+{
+    public partial class addtag : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ResumeTags");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Resumes",
+                type: "nvarchar(4000)",
+                maxLength: 4000,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkingStyle",
+                table: "Jobs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MilitaryStatus",
+                table: "Jobs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EducationLevel",
+                table: "Jobs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Resumes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WorkingStyle",
+                table: "Jobs",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MilitaryStatus",
+                table: "Jobs",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EducationLevel",
+                table: "Jobs",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ResumeTags",
+                columns: table => new
+                {
+                    ResumeId = table.Column<string>(nullable: false),
+                    TagId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResumeTags", x => new { x.ResumeId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_ResumeTags_Resumes_ResumeId",
+                        column: x => x.ResumeId,
+                        principalTable: "Resumes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ResumeTags_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResumeTags_TagId",
+                table: "ResumeTags",
+                column: "TagId");
+        }
+    }
+}
