79bdabea30c72e1c8d44c28d8221a3614669ff41
Elif Sevgili <elifsevgili18@gmail.com>
2017-11-23 14:36:47 +0300

    job IsActive

--- HrPortal/Controllers/JobsController.cs
+++ HrPortal/Controllers/JobsController.cs
@@ -33,13 +33,14 @@ namespace HrPortal.Controllers
         public async Task<IActionResult> Index(JobSearchViewModel jvm)
         {
             
-            jvm.SearchResults = await jobRepository.GetPaged(s => s.PublishDate<=DateTime.Now && DateTime.Now<=s.EndDate&&(!String.IsNullOrEmpty(jvm.Keywords) ? s.Title.Contains(jvm.Keywords) : true) && (!String.IsNullOrEmpty(jvm.LocationId) ? s.JobLocations.Any(l=> l.LocationId == jvm.LocationId) : true) && (jvm.MilitaryStatus.HasValue ? s.MilitaryStatus == jvm.MilitaryStatus : true) && (jvm.EducationLevel.HasValue ? s.EducationLevel == jvm.EducationLevel : true)&& (jvm.WorkingStyle.HasValue ? s.WorkingStyle == jvm.WorkingStyle : true), s => (jvm.SortBy == 1 || jvm.SortBy == 2 ? s.Title : (jvm.SortBy == 3 || jvm.SortBy == 4 ? s.Occupation.Name : (jvm.SortBy == 5 || jvm.SortBy == 6 ? s.Company.LocationId: s.UpdateDate.ToString()))), (jvm.SortBy == 1 || jvm.SortBy == 3 || jvm.SortBy == 5 ? false : (jvm.SortBy == 2 || jvm.SortBy == 4 || jvm.SortBy == 6)),5,jvm.Page, "Company", "JobLocations", "JobLocations.Location");
+            jvm.SearchResults = await jobRepository.GetPaged(s => s.IsActive == true && s.PublishDate<=DateTime.Now && DateTime.Now<=s.EndDate&&(!String.IsNullOrEmpty(jvm.Keywords) ? s.Title.Contains(jvm.Keywords) : true) && (!String.IsNullOrEmpty(jvm.LocationId) ? s.JobLocations.Any(l=> l.LocationId == jvm.LocationId) : true) && (jvm.MilitaryStatus.HasValue ? s.MilitaryStatus == jvm.MilitaryStatus : true) && (jvm.EducationLevel.HasValue ? s.EducationLevel == jvm.EducationLevel : true)&& (jvm.WorkingStyle.HasValue ? s.WorkingStyle == jvm.WorkingStyle : true), s => (jvm.SortBy == 1 || jvm.SortBy == 2 ? s.Title : (jvm.SortBy == 3 || jvm.SortBy == 4 ? s.Occupation.Name : (jvm.SortBy == 5 || jvm.SortBy == 6 ? s.Company.LocationId: s.UpdateDate.ToString()))), (jvm.SortBy == 1 || jvm.SortBy == 3 || jvm.SortBy == 5 ? false : (jvm.SortBy == 2 || jvm.SortBy == 4 || jvm.SortBy == 6)),5,jvm.Page, "Company", "JobLocations", "JobLocations.Location");
             jvm.SearchResults.RouteValue = new RouteValueDictionary { { "keywords", jvm.Keywords }, { "locationId", jvm.LocationId }, { "sortBy", jvm.SortBy }, { "militaryStatus", jvm.MilitaryStatus }, { "educationLevel", jvm.EducationLevel }, { "workingStyle", jvm.WorkingStyle } };
             ViewBag.Locations = new SelectList(locationRepository.GetAll().OrderBy(o => o.Name).ToList(), "Id", "Name", jvm.LocationId);
             ViewBag.Occupations = new SelectList(occupationRepository.GetAll().OrderBy(p => p.Name).ToList(), "Id", "Name", jvm.OccupationId);
             return View(jvm);
 
         }
+        [Authorize(Roles = "Employer,Admin")]
         public IActionResult Create()
         {
             var job = new Job();
@@ -51,6 +52,7 @@ namespace HrPortal.Controllers
 
         }
         [HttpPost]
+        [Authorize(Roles = "Employer,Admin")]
         public IActionResult Create(Job job)
         {
             if (ModelState.IsValid)
@@ -74,7 +76,6 @@ namespace HrPortal.Controllers
         {
             var job = jobRepository.Get(id, "Company", "JobLocations", "JobLocations.Location");
             ViewBag.PublishAgoFormat = DisplayAgoFormat(job.PublishDate);
-            ViewBag.IsActiveFormat = IsActiveFormat(job.PublishDate);
             return View(job);
            
         }
@@ -102,6 +103,7 @@ namespace HrPortal.Controllers
             return View(jobApplication);
         }
 
+        [Authorize(Roles = "Employer,Admin")]
         public IActionResult Edit(string id)
         {
 
@@ -112,6 +114,7 @@ namespace HrPortal.Controllers
             return View(job);
         }
         [HttpPost]
+        [Authorize(Roles = "Employer,Admin")]
         public IActionResult Edit(Job job)
         {
             if (ModelState.IsValid)
@@ -128,12 +131,22 @@ namespace HrPortal.Controllers
             return View(job);
         }
 
-        public IActionResult Remove(string id)
+        [Authorize(Roles = "Employer,Admin")]
+        public IActionResult Delete(string id)
         {
             var job = jobRepository.Get(id);
             ViewBag.Companies = new SelectList(companyRepository.GetAll().OrderBy(c => c.Name).ToList(), "Id", "Name");
             ViewBag.Locations = locationRepository.GetAll().OrderBy(l => l.Name).ToList();
             jobRepository.Delete(job);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Remove(string id)
+        {
+            var job = jobRepository.Get(id);
+            job.IsActive = false;
+            jobRepository.Update(job);
+         
             return RedirectToAction("Index");
         }
         public IActionResult SuccessfullyCreated()
@@ -176,25 +189,7 @@ namespace HrPortal.Controllers
             
        
        }
-        static string IsActiveFormat(DateTime inputDate)
-        {
-            DateTime date = DateTime.Today;
-            TimeSpan interval = date - inputDate;
-            if (interval.TotalDays < 60)
-            {
-                string result = "Evet";
-                return result;
-            }
-            else
-            {
-                string result = "Hayır";
-                return result;
-
-
-            }
-
-        }
-       
+        
 
     }
 }
