db04bdcfe575d55a6e3b7a9391a74588a4f5579d
ezgisahin <ezgisahin87@gmail.com>
2018-01-02 12:08:46 +0300

    add-occupation-migration

---
+++ HrPortal/Migrations/20180102090643_occupation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HrPortal.Migrations
+{
+    public partial class occupation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ResumeId",
+                table: "JobApplications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ResumeId",
+                table: "JobApplications",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
